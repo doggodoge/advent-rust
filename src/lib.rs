71a59968c91ab5d1624c107dd062b9d4e6445f33
Gary Moore <madmangaz@gmail.com>
2022-05-29 03:32:32 +0100

    feat: add time func for benchmarking funcs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
 pub mod sonar_sweep;
+
+pub fn time<F, R>(func: F) -> (R, std::time::Duration)
+where
+    F: Fn() -> R,
+{
+    let start = std::time::Instant::now();
+    let result = func();
+    let end = std::time::Instant::now();
+    let duration = end - start;
+    (result, duration)
+}
--- src/sonar_sweep/mod.rs
+++ src/sonar_sweep/mod.rs
@@ -1,6 +1,8 @@
 pub use puzzle_one::puzzle_one;
 pub use puzzle_two::puzzle_two;
 
+use crate::time;
+
 mod puzzle_one;
 mod puzzle_two;
 mod util;
@@ -8,7 +10,18 @@ mod util;
 pub fn print_solutions() {
     let data = include_str!("data/data.txt");
 
+    let (puzzle_one_result, puzzle_one_duration) = time(|| puzzle_one(data));
+    let (puzzle_two_result, puzzle_two_duration) = time(|| puzzle_two(data));
+
     println!("Sonar Sweep (day one) Solutions:");
-    println!("\nPuzzle one: {}", puzzle_one(data));
-    println!("\nPuzzle two: {}", puzzle_two(data));
+    println!(
+        "\tPuzzle one: {} ({}μs)",
+        puzzle_one_result,
+        puzzle_one_duration.as_micros()
+    );
+    println!(
+        "\tPuzzle two: {} ({}μs)",
+        puzzle_two_result,
+        puzzle_two_duration.as_micros()
+    );
 }
